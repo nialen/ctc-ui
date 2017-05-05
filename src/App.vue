be17f6f622c70e758a988033ccfe7b9273fc2b92
dingshaohua <345561171@qq.com>
2017-05-05 11:34:14 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <el-button type="primary">主要按钮</el-button>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
